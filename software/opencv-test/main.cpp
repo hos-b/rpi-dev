367b88b2fde72434b0ac7b70bc635207a2652b4d
Hosein <bahadorm@informatik.uni-freiburg.de>
2021-01-15 15:01:21 +0100

    actually use opencv inside the test

--- software/opencv-test/main.cpp
+++ software/opencv-test/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
 int main()
 {
+    cv::Mat hello_mat(640, 480, CV_8UC3);
+    cv::putText(hello_mat, "Hello?", cv::Point(300, 200), cv::FONT_HERSHEY_COMPLEX, 1.0, cv::Scalar(255, 127, 0));
+    cv::imwrite("hello.png", hello_mat);
     std::cout << "cross-compiled in aarch64 container" << std::endl;
     return 0;
 }
